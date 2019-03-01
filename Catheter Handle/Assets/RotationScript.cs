4f842b55bedd3ca5930681cb6458078d0198d0b9
Janae Chan <jchan15@cs.washington.edu>
2019-03-01 13:58:28 -0800

    remove assets, add pi's ip addr

--- Catheter Handle/Assets/RotationScript.cs
+++ Catheter Handle/Assets/RotationScript.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using System.IO.Ports;
 
 public class RotationScript : MonoBehaviour
 {
 	public float moveSpeed = 10f;
 	public float turnSpeed = 50f;
+	SerialPort stream = new SerialPort("/dev/cu.applepi0-SerialPort", 9600);
 
 	void Start()
 	{
 		Debug.Log("Hello World");
+		// Get a list of serial port names.
+		string[] ports = SerialPort.GetPortNames();
+
+		Debug.Log("The following serial ports were found:");
+		// Display each port name to the console.
+		foreach(string port in ports)
+		{
+			Debug.Log(port);
+		}
 	}
 	void Update ()
 	{
+//		string value = stream.ReadLine(); //Read the information
+//		print(value);
+
 		if(Input.GetKey(KeyCode.UpArrow))
 			transform.Rotate(Vector3.left * -Time.deltaTime * turnSpeed, Space.World);
 
