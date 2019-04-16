5bd7cc06910dfba9f2b593cab9147bface5398ea
Janae Chan <jchan15@cs.washington.edu>
2019-04-15 19:04:43 -0700

    identify device

--- Catheter Handle/Assets/RotationScript.cs
+++ Catheter Handle/Assets/RotationScript.cs
@@ -1,32 +1,62 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 using System.IO.Ports;
+
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Button))]
 
 public class RotationScript : MonoBehaviour
 {
 	public float moveSpeed = 10f;
 	public float turnSpeed = 50f;
-	SerialPort stream = new SerialPort("/dev/cu.applepi0-SerialPort", 9600);
+	SerialPort stream = new SerialPort("/dev/cu.usbmodem1411", 9600);
+
+	Button button;
+	Image image;
+
+	public ButtonType buttonType;
 
 	void Start()
 	{
 		Debug.Log("Hello World");
+		stream.Open(); //Open the Serial Stream.
 		// Get a list of serial port names.
-		string[] ports = SerialPort.GetPortNames();
-
+		Debug.Log(SerialPort.GetPortNames());
 		Debug.Log("The following serial ports were found:");
 		// Display each port name to the console.
-		foreach(string port in ports)
+		foreach(string port in SerialPort.GetPortNames())
 		{
-			Debug.Log(port);
+			Debug.Log("___"+port);
 		}
+
 	}
+	void OnGUI()
+	{
+		GUI.Box(new Rect(0, 0, 50, 50), "This is a box");
+	}
+
+	protected override void OnSkinUI()
+	{
+
+		base.OnSkinUI();
+		image = GetComponent<Image>();
+		button = GetComponent<Button>();
+
+		button.transition = Selectable.Transition.SpriteSwap;
+		button.targetGraphic = image;
+
+		image.sprite = skinData.buttonSprite;
+		image.type = Image.Type.Sliced;
+		button.spriteState = skinData.buttonSpriteState;
+	}
+
 	void Update ()
 	{
 //		string value = stream.ReadLine(); //Read the information
 //		print(value);
-
+//
 		if(Input.GetKey(KeyCode.UpArrow))
 			transform.Rotate(Vector3.left * -Time.deltaTime * turnSpeed, Space.World);
 
